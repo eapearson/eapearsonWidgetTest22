a7750889dcd9f369e6ca24c0c653721ae1674e1a
Erik Pearson <eapearson@lbl.gov>
2024-02-01 12:56:39 -0800

    Initialized service with 'make all'

--- lib/eapearsonWidgetTest22/eapearsonWidgetTest22Impl.py
+++ lib/eapearsonWidgetTest22/eapearsonWidgetTest22Impl.py
@@ -1,18 +1,56 @@
+# -*- coding: utf-8 -*-
 #BEGIN_HEADER
 import logging
 import os
 
 from installed_clients.KBaseReportClient import KBaseReport
 #END_HEADER
-#BEGIN_CLASS_HEADER
-#END_CLASS_HEADER
-#BEGIN_CONSTRUCTOR
+
+
+class eapearsonWidgetTest22:
+    '''
+    Module Name:
+    eapearsonWidgetTest22
+
+    Module Description:
+    A KBase module: eapearsonWidgetTest22
+    '''
+
+    ######## WARNING FOR GEVENT USERS ####### noqa
+    # Since asynchronous IO can lead to methods - even the same method -
+    # interrupting each other, you must be *very* careful when using global
+    # state. A method could easily clobber the state set by another while
+    # the latter method is running.
+    ######################################### noqa
+    VERSION = "0.0.1"
+    GIT_URL = ""
+    GIT_COMMIT_HASH = "2cfa706382e30e1bc5aee8a443ef0cf413573fa6"
+
+    #BEGIN_CLASS_HEADER
+    #END_CLASS_HEADER
+
+    # config contains contents of config file in a hash or None if it couldn't
+    # be found
+    def __init__(self, config):
+        #BEGIN_CONSTRUCTOR
         self.callback_url = os.environ['SDK_CALLBACK_URL']
         self.shared_folder = config['scratch']
         logging.basicConfig(format='%(created)s %(levelname)s: %(message)s',
                             level=logging.INFO)
-#END_CONSTRUCTOR
-#BEGIN run_eapearsonWidgetTest22
+        #END_CONSTRUCTOR
+        pass
+
+
+    def run_eapearsonWidgetTest22(self, ctx, params):
+        """
+        This example function accepts any number of parameters and returns results in a KBaseReport
+        :param params: instance of mapping from String to unspecified object
+        :returns: instance of type "ReportResults" -> structure: parameter
+           "report_name" of String, parameter "report_ref" of String
+        """
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN run_eapearsonWidgetTest22
         report = KBaseReport(self.callback_url)
         report_info = report.create({'report': {'objects_created':[],
                                                 'text_message': params['parameter_1']},
@@ -21,11 +59,20 @@ from installed_clients.KBaseReportClient import KBaseReport
             'report_name': report_info['name'],
             'report_ref': report_info['ref'],
         }
-#END run_eapearsonWidgetTest22
-#BEGIN_STATUS
+        #END run_eapearsonWidgetTest22
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, dict):
+            raise ValueError('Method run_eapearsonWidgetTest22 return value ' +
+                             'output is not type dict as required.')
+        # return the results
+        return [output]
+    def status(self, ctx):
+        #BEGIN_STATUS
         returnVal = {'state': "OK",
                      'message': "",
                      'version': self.VERSION,
                      'git_url': self.GIT_URL,
                      'git_commit_hash': self.GIT_COMMIT_HASH}
-#END_STATUS
+        #END_STATUS
+        return [returnVal]
